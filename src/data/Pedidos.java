3589fdc3e3dd9f749bab0bb414dd43d31ad227c7
Paulo Augusto <paulomeissner7@gmail.com>
2024-04-26 18:36:03 -0300

    adicionando atributos

--- src/data/Pedidos.java
+++ src/data/Pedidos.java
@@ -3,13 +3,68 @@ package data;
 import java.util.ArrayList;
 
 public class Pedidos {
-    private Funcionario f;
+    private Usuario u;
+    //private Departamento d; Adicionar depois da implementação da classe Departamento
     private String data;
     private String dataDeConclusao;
     private Status status;
     private ArrayList<Item> lista;
     private double valorDoPedido;
     
-    
+
+    public Pedidos(Usuario u, String data, String dataDeConclusao, Status status, ArrayList<Item> lista, double valorDoPedido) {
+        this.u = u;
+        this.data = data;
+        this.dataDeConclusao = dataDeConclusao;
+        this.status = status;
+        this.lista = lista;
+        this.valorDoPedido = valorDoPedido;
+    }
+
+    public Usuario getU() {
+        return this.u;
+    }
+
+    public void setU(Usuario u) {
+        this.u = u;
+    }
+
+    public String getData() {
+        return this.data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getDataDeConclusao() {
+        return this.dataDeConclusao;
+    }
+
+    public void setDataDeConclusao(String dataDeConclusao) {
+        this.dataDeConclusao = dataDeConclusao;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public void setStatus(Status status, Usuario u) {
+        // adicionar verificaçao para alterar status
+        this.status = status;
+    }
+
+    public ArrayList<Item> getLista() {
+        return this.lista;
+    }
+
+    public void setLista(ArrayList<Item> lista) {
+        this.lista = lista;
+    }
+
+    public double getValorDoPedido() {
+        return this.valorDoPedido;
+    }
+
     
 }
