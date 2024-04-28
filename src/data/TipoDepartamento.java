8191f31241ecf6f4cbcc13bf3d879f193405e3f3
ViniciusMatox <vrmatos13@gmail.com>
2024-04-28 20:58:39 -0300

    Criação da classe Departamento

---
+++ src/data/TipoDepartamento.java
@@ -0,0 +1,25 @@
+package data;
+public enum TipoDepartamento{
+    
+    FINANCEIRO("Financeiro", 10000.00), 
+    JURIDICO("Jurídico", 10000.00), 
+    RECURSOS_HUMANOS("Recursos Humanos", 10000.00), 
+    ENGENHARIA("Engenharia", 10000.00), 
+    MANUTENCAO("Manutenção", 10000.00);
+
+    private final String nome;
+    private final double valorMaximoPedido;
+
+    TipoDepartamento(String nome, double valorMaximoPedido){
+        this.nome = nome;
+        this.valorMaximoPedido = valorMaximoPedido;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+    public double getValorMaximoPedido(){
+        return valorMaximoPedido;
+    }
+}
