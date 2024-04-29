47acda5aa6ec743985c72f33938f74ed346e05bc
Paulo Augusto <paulomeissner7@gmail.com>
2024-04-28 22:50:01 -0300

    implementando classe Pedido

---
+++ src/data/ListaDePedidos.java
@@ -0,0 +1,12 @@
+package data;
+
+import java.util.ArrayList;
+
+public class ListaDePedidos {
+    private ArrayList<Pedido> listaPedidos;
+
+    public ListaDePedidos(){
+        listaPedidos = new ArrayList<>();
+    }
+    
+}
--- src/data/Pedido.java
+++ src/data/Pedido.java
@@ -4,37 +4,35 @@ import java.util.ArrayList;
 
 public class Pedido {
     private Usuario u;
-    //private Departamento d; Adicionar depois da implementação da classe Departamento
+    private int id;
     private String data;
     private String dataDeConclusao;
     private Status status;
     private ArrayList<Item> lista;
     private double valorDoPedido;
+    private TipoDepartamento departamento;
     
 
-    public Pedido(Usuario u, String data, String dataDeConclusao, Status status, ArrayList<Item> lista, double valorDoPedido) {
+    public Pedido(Usuario u, int id, String data, String dataDeConclusao, Status status, ArrayList<Item> lista) {
         this.u = u;
+        this.id = id;
         this.data = data;
         this.dataDeConclusao = dataDeConclusao;
         this.status = status;
         this.lista = lista;
-        this.valorDoPedido = valorDoPedido;
+        this.valorDoPedido = 0;
+        this.departamento = u.getDepartamento().getNome();
     }
 
     public Usuario getU() {
         return this.u;
     }
-
-    public void setU(Usuario u) {
-        this.u = u;
+    public int getId(){
+        return id;
     }
 
     public String getData() {
         return this.data;
-    }
-
-    public void setData(String data) {
-        this.data = data;
     }
 
     public String getDataDeConclusao() {
@@ -49,9 +47,17 @@ public class Pedido {
         return this.status;
     }
 
-    public void setStatus(Status status, Usuario u) {
-        // adicionar verificaçao para alterar status
-        this.status = status;
+    public boolean setStatus(Status status, Usuario u) {
+        if(this.status.equals(Status.CONCLUIDO)){
+            return false;
+        }
+        if(status.equals(Status.APROVADO)||status.equals(Status.REPROVADO)){
+            if(u instanceof Administrador){
+                this.status = status;
+                return true;
+            }
+        }
+       return false;
     }
 
     public ArrayList<Item> getLista() {
@@ -66,5 +72,17 @@ public class Pedido {
         return this.valorDoPedido;
     }
 
+    public void setValorDoPedido(double valor){
+        this.valorDoPedido = this.valorDoPedido + valor;
+    }
+
+    public boolean addItem(Item i){
+        	if((getValorDoPedido()+(i.getValorUnitario()))>(u.getDepartamento().getNome().getValorMaximoPedido())){
+                return false;
+            }
+            lista.add(i);
+            setValorDoPedido(i.getValorUnitario());
+            return true;
+    }
     
 }
